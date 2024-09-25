a46f2db8015f17e121d77560c3404655f2266ecd
Anil <anil@123>
2024-09-25 14:43:57 +0530

    aa

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM openjdk:17
+
+COPY target/Finance-0.0.1-SNAPSHOT.jar  /usr/app/
+
+WORKDIR /usr/app/
+
+ENTRYPOINT ["java", "-jar", "Finance-0.0.1-SNAPSHOT.jar"]
+
+EXPOSE 8080
+
+
---
+++ src/main/java/com/nt/admin/AdminController.java
@@ -0,0 +1,14 @@
+package com.nt.admin;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+	
+	@GetMapping("msg")
+	public String home() {
+		return "JNBDB";
+	}
+
+}
